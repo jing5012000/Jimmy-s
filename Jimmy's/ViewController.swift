910521980f1a518745024da88b3bc2434c47c138
Lucy <lucy@LucydeMacBook-Air.local>
2017-07-05 18:44:56 +0800

    Initial Commit

---
+++ Jimmy's/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jimmy's
+//
+//  Created by Lucy on 2017/7/5.
+//  Copyright © 2017年 Lucy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
